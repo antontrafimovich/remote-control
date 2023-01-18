9612d56a7481fd76a5d168e17c8493ab8f2a3556
Anton Trafimovich <antont@compatibl.com>
2023-01-18 19:57:21 +0100

    init: add basic websocket server.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { wsServer } from "./src/ws_server/index.js";
+
+
+const WS_PORT = 8080;
+wsServer.listen(WS_PORT);
 
 const HTTP_PORT = 8181;
-
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
